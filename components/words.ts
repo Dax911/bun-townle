e48389436fbe4e86364306e64ee0d3c95f475cd6
Dax911 <haydenaylor@icloud.com>
2022-07-11 17:26:50 -0500

    bun init

---
+++ components/words.ts
@@ -0,0 +1,22 @@
+
+const wordlists = {
+    3: [
+        'Ufa',
+        'Man',
+    ],
+    4: [
+        'Keiv',
+        'Oslo',
+        'Omsk',
+    ],
+    5: [
+        'China',
+        'India',
+
+    ],
+    6: [
+        'Boston',
+        'Bangor',
+        'Panama'
+    ]
+}
